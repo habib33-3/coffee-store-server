f67f9dbd772f967da755f680902287e136f2d19b
Habibur Rahman <habib.swi16@gmail.com>
2023-10-13 22:18:10 +0600

    display datas from the server

--- index.js
+++ index.js
@@ -36,6 +36,12 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/coffee", async (req, res) => {
+      const cursor = coffeeCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
