99e558864e861c038fc0ddd2b484787dbe6dc011
Habibur Rahman <habib.swi16@gmail.com>
2023-10-13 22:09:55 +0600

    send data data to mongodb

--- index.js
+++ index.js
@@ -25,6 +25,17 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const coffeeCollection = client.db("coffeeDB").collection("coffee");
+
+    app.post("/coffee", async (req, res) => {
+      const newCoffee = req.body;
+      // console.log(newCoffee);
+      const result = await coffeeCollection.insertOne(newCoffee);
+      console.log(result);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
